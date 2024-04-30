a27f50a9b1c11d1326e575db5cd51ead76bb8629
Ozias Dushimimana <odushimi@gmail.com>
2024-04-30 16:33:18 -0400

    Add Factorial in Math

--- MyProjects/Math.cs
+++ MyProjects/Math.cs
@@ -8,4 +8,11 @@ public static class Math
         if (number < 2) return number;
         return Fibonacci(number - 2) + Fibonacci (number - 1);
     }
+
+    public static int Factorial(int number)
+    {
+        if (number < 0) throw new Exception("Number cannot be less than 0.");
+        if (number < 2) return 1;
+        return Factorial(number -1 ) * number;
+    }
 }
---
+++ TestMyProjects/TestMath/TestFactorial.cs
@@ -0,0 +1,39 @@
+﻿using static MyProjects.Math;
+
+namespace TestMyProjects;
+
+public class TestFactorial
+{
+    [Theory]
+    [InlineData(0, 1)]
+    [InlineData(1, 1)]
+    [InlineData(2, 2)]
+    [InlineData(3, 6)]
+    [InlineData(4, 24)]
+    [InlineData(5, 120)]
+    [InlineData(6, 720)]
+    [InlineData(7, 5040)]
+    [InlineData(8, 40320)]
+    //[InlineData(18, 6402373705728000)]
+    public void ShouldCalculateFactorial(int number, int expected)
+    {
+        var actual = Factorial(number);
+
+        Assert.True(actual == expected,
+        $"Expected Factorial({number})={expected}");
+    }
+
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(-2)]
+    [InlineData(-3)]
+    [InlineData(-99)]
+    public void ShouldThrowException(int number)
+    {
+        Func<object?> testCode = () => Factorial(number);
+        var exception = Assert.Throws<Exception>(testCode);
+
+        Assert.Equal("Number cannot be less than 0.", exception.Message);
+    }
+}
